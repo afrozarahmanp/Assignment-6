943b32f7e9138eb87dff1764579d70b65fe4ef63
afrozarahmanp <afroza.rahman.p@gmail.com>
2023-03-19 17:19:27 +0600

    bootstrap added

---
+++ main.js
@@ -0,0 +1,10 @@
+const aiHubData = async() => {
+const url = `https://openapi.programming-hero.com/api/ai/tools`;
+const res = await fetch(url);
+const data = await res.json();
+console.log(data.data.tools);
+}
+
+
+
+aiHubData();
