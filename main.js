47db9a0614681c02bd5f850ffe6fb43a63015fe7
afrozarahmanp <afroza.rahman.p@gmail.com>
2023-03-20 18:03:19 +0600

    Doing Modal part (Subscription)

--- main.js
+++ main.js
@@ -72,15 +72,6 @@ console.log(aiDatas);
 
 aiHubData();
 
-// const aiHubModal = id
-const aiHubModalData = async (id) => {
-    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
-    const res = await fetch(url);
-    const data = await res.json();
-console.log(data);
-
-}
-
 
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader')
@@ -105,3 +96,81 @@ document.getElementById('sort-btn').addEventListener('click', function(){
     aiHubData(2);
 
 })
+
+const aiHubModalData = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+    aiModal(data.data)
+}
+aiHubModalData('01');
+
+const aiModal = (modalData) => {
+    const aiHubModal = document.getElementById('ai-hub-modal');
+    aiHubModal.textContent = '';
+
+    const modalDiv1 = document.createElement('div');
+    modalDiv1.classList.add('col');
+        modalDiv1.innerHTML = `
+        <div class="card border border-danger bg-danger-subtle opacity-75">
+        <h4 class=" text-black card-body ">${modalData.description}</h4>
+        <div class="card-body">
+        //-------------------------------------------- subscription----------------------------------------------------------------------
+          <div id="subscription" class="row row-cols-1 row-cols-md-3 g-3">
+
+          </div>
+          <div class="d-flex justify-content-between m-3">
+            <div>
+              <h4 class=" text-black card-title mt-3">Card title</h4>
+              <ul>
+                <li>blah</li>
+                <li>blah</li>
+                <li>blah</li>
+              </ul>
+            </div>
+            <div>
+              <h4 class=" text-black card-title mt-3 ">Card title</h4>
+              <ul>
+                <li>blah</li>
+                <li>blah</li>
+                <li>blah</li>
+              </ul>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+        `
+    
+
+
+
+}
+
+const subscriptionDetails = (modalData) => {
+
+    const subscription = document.getElementById('subscription');
+    subscription.innerHTML = `
+            <div class="col">
+              <div class="card border border-secondary-subtle bg-white text-center">
+                <p class="mt-3 text-success fw-bold">${modalData.pricing[0].price? modalData.pricing[0] : "Free subscription"}
+                <br> ${modalData.pricing[0].plan? modalData.pricing[0].plan : "/Basic"} </p>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card border border-secondary-subtle bg-white text-center">
+                <p class="mt-3 text-warning fw-bold">${modalData.pricing[1].price? modalData.pricing[1] : "Free subscription"}
+                <br> ${modalData.pricing[1].plan? modalData.pricing[1].plan : "/Pro"}</p>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card border border-secondary-subtle bg-white text-center">
+                <p class="mt-3 text-danger fw-bold">${modalData.pricing[2].price? modalData.pricing[2] : "Free subscription"}
+                <br> ${modalData.pricing[2].plan? modalData.pricing[2].plan : "/Enterprise"} </p>
+              </div>
+            </div>
+    `;
+
+}
